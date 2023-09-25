481a5ac322513a2af40def95c94ab1f4deb0f972
rashednyn <rashedmahamud22@gmail.com>
2023-09-26 01:10:00 +0600

    creating all environment and pages

---
+++ src/Pages/Statistics/Statistics.jsx
@@ -0,0 +1,11 @@
+
+
+const Statistics = () => {
+    return (
+        <div>
+            <h2>This is Stat Page</h2>
+        </div>
+    );
+};
+
+export default Statistics;
---
+++ src/Router/Route.jsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layout/MainLayout";
+import Home from "../Pages/Home/Home";
+import Donations from "../Pages/Donations/Donations";
+import Statistics from "../Pages/Statistics/Statistics";
+
+const myCreatedRoute = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/donations",
+        element: <Donations></Donations>,
+      },
+      {
+        path: "/statistics",
+        element: <Statistics></Statistics>,
+      },
+    ],
+  },
+]);
+export default myCreatedRoute;
